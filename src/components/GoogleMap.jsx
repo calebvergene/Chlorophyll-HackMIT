da3b3db54ddf86c9eb179d07237fff6fa3acb45b
caleb vergene <calebverg@gmail.com>
2024-09-14 15:40:23 -0700

    make markers for each landmark

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -1,14 +1,20 @@
-import { AdvancedMarker, Map } from "@vis.gl/react-google-maps";
-import { useState } from "react";
+import React from 'react';
+import { AdvancedMarker, Map, Pin } from "@vis.gl/react-google-maps";
+import landmarks from '../UHILandmarks.json';  // Import the JSON data
 
 export default function GoogleMap() {
-  const [coords, setCoords] = useState({ lat: 42.3601, lng: -71.0942 });
 
   return (
     <div className="h-screen w-screen">
-      {/* Can customize how map looks with map ID*/}
-      <Map defaultCenter={coords} defaultZoom={10} mapId="DEMO_MAP_ID">
-        <AdvancedMarker position={coords} />
+      <Map defaultCenter={{ lat: 42.3601, lng: -71.0942 }} defaultZoom={10} mapId="DEMO_MAP_ID" gestureHandling={'greedy'} disableDefaultUI={true}>
+        {landmarks.map((landmark, index) => (
+          <AdvancedMarker
+            key={index}
+            position={{ lat: landmark.lat, lng: landmark.long }}  // Use coordinates from the JSON
+          >
+            <Pin background={'#68be5e'} glyphColor={'#027'} borderColor={'#000'} />
+          </AdvancedMarker>
+        ))}
       </Map>
     </div>
   );
