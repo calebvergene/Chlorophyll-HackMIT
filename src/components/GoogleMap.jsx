e7916ac9affba764c08535c3b1c38c6f56a95a79
caleb vergene <calebverg@gmail.com>
2024-09-14 18:50:28 -0700

    updatye

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -69,7 +69,7 @@ export default function GoogleMap() {
             animate={{ x: 0, opacity: 1 }}
             exit={{ x: "-100%", opacity: 0 }}
             transition={{ duration: 0.5, ease: [0.34, 1, 0.64, 1] }}
-            className="fixed top-0 left-0 h-full w-1/3 bg-white shadow-lg z-50 p-6 pl-8 text-black/90 overflow-auto rounded-r-3xl"
+            className="fixed top-0 left-0 h-full w-1/3 bg-white shadow-lg z-50 p-6 px-8 text-black/90 overflow-auto rounded-r-3xl"
           >
             <div className="mb-6">
               <div className="flex justify-between items-center mb-3 w-full">
@@ -109,7 +109,26 @@ export default function GoogleMap() {
                 {selectedLandmark["Annual nighttime SUHI"]}
               </p>
             </div>
-            <h2 className="font-bold text-3xl">Street View</h2>
+            <div className="flex justify-between items-center mb-4 mt-7">
+              <h2 className="font-bold text-3xl">Street View</h2>
+              <button className="bg-emerald-600 text-white rounded-md p-2 px-7 pl-5 font-semibold flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6 mr-2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z"
+                  />
+                </svg>
+                Greenify with AI
+              </button>
+            </div>
             <div className="flex flex-wrap">
               {/* Displaying the even-indexed images */}
               {imagesToShow.map((url, index) => (
@@ -117,7 +136,7 @@ export default function GoogleMap() {
                   key={index}
                   src={url}
                   alt={`Street View Heading ${index * 45}`}
-                  className="my-1 w-2/5 h-auto object-cover" // Adjust size here
+                  className="my-1 w-6/12 h-auto object-cover" // Adjust size here
                 />
               ))}
             </div>
@@ -131,6 +150,7 @@ export default function GoogleMap() {
                 {showAll ? "Show Less" : "Show More"}
               </button>
             )}
+            
           </motion.div>
         )}
       </AnimatePresence>
